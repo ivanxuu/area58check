d626080bd9fa9aeff8a4512380ed2e303941dad9
Ivan H <ivan@drails.com>
2018-03-03 08:48:56 +0100

    Initial

---
+++ lib/area58check.ex
@@ -0,0 +1,18 @@
+defmodule Area58check do
+  @moduledoc """
+  Documentation for Area58check.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Area58check.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/area58check_test.exs
@@ -0,0 +1,8 @@
+defmodule Area58checkTest do
+  use ExUnit.Case
+  doctest Area58check
+
+  test "greets the world" do
+    assert Area58check.hello() == :world
+  end
+end
