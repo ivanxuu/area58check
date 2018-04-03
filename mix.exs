b61dc763985d22e32e2b32fe36db59c7f033ee6f
Ivan H <ivan@drails.com>
2018-04-03 19:43:45 +0200

    Add headers to `mix.exs` to publish library on hex.pm
    I want to publish this library in hex.pm, so added all parameters I
    think are needed to do so.
    
    Also added library ExDocs to enable documentation.

--- mix.exs
+++ mix.exs
@@ -1,13 +1,19 @@
 defmodule Area58check.Mixfile do
   use Mix.Project
 
+  @github_url "https://github.com/ihinojal/area58check"
+
   def project do
     [
       app: :area58check,
       version: "0.1.0",
       elixir: "~> 1.5",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      source_url: @github_url,
+      docs: docs()
     ]
   end
 
@@ -18,12 +24,37 @@ defmodule Area58check.Mixfile do
     ]
   end
 
+  #── PRIVATE ──────────────────────────────────────────────────────────
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description do
+    "Base58check library to encode binary data or decode encoded "<>
+    "strings. It's used in Bitcoin whenever there is a need for a "<>
+    "user to read or transcribe a number, such a bitcoin adresses, "<>
+    "encrypted key, private key, or script hash."
+  end
+
+  defp package do
+    [maintainers: ["Ivan H."],
+     licenses: ["MIT"],
+     links: %{"Github" => @github_url}]
+  end
+
+  defp docs do
+    [
+      main: "Area58check",
+      # source_ref: "v#{@version}",
+      # logo: "path/to/logo.png",
+      extras: ["README.md"]
     ]
   end
 end
