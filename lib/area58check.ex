00ae7e49cc978b9d717e7a99f270efddb5efd5dc
Ivan H <ivan@drails.com>
2018-04-03 17:08:39 +0200

    Add missing documentation to the main module
    There was no documentation of the public API for the usage of this
    library.

--- lib/area58check.ex
+++ lib/area58check.ex
@@ -1,6 +1,45 @@
 defmodule Area58check do
   @moduledoc """
+  This module includes the needed functions to encode or decode
+  base58check strings.
 
+  Base58check is used in Bitcoin whenever there is a need for a user to
+  read or transcribe a number, such a bitcoin adresses, encrypted key,
+  private key, or script hash.
+
+  ## Prefixed versions
+  The prefixed version parameter indicates what kind of binary is being
+  encoded. E.g. If you are encoding a private key use `:wif`, if you are
+  encoding a bitcoin address use `:p2pkh`. The version prefix will be
+  embeded in the encoded version.
+
+  ## Checksum
+  The encoded versions will include a few bytes to ensure that its
+  correct and no transcribing errors were present.
+
+  ## Usage
+
+  To know the **decoded binary** of any encoded string just use the
+  `encode/2` function indicating the prefix version you want:
+
+      iex> privkey = "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" |> Base.decode16!()
+      iex> %{decoded: _decoded} = encode(privkey, version: <<128>>)
+      %Area58check{
+        encoded: "5HpneLQNKrcznVCQpzodYwAmZ4AoHeyjuRf9iAHAa498rP5kuWb",
+        decoded: <<1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69, 103, 137, 171, 205, 239>>,
+        version: :wif,
+        version_bin: <<128>>
+      }
+
+  To know the **encoded string** of any binary use `decode/1` function:
+
+      iex> {:ok, %{encoded: _encoded}} = decode("5HpneLQNKrcznVCQpzodYwAmZ4AoHeyjuRf9iAHAa498rP5kuWb")
+      {:ok, %Area58check{
+        encoded: "5HpneLQNKrcznVCQpzodYwAmZ4AoHeyjuRf9iAHAa498rP5kuWb",
+        decoded: <<1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69, 103, 137, 171, 205, 239, 1, 35, 69, 103, 137, 171, 205, 239>>,
+        version: :wif,
+        version_bin: <<128>>
+      }}
   """
   alias __MODULE__.{Prefixes, Encoder, Decoder}
 
@@ -15,6 +54,12 @@ defmodule Area58check do
   @default_options []
 
   @doc """
+  Encodes a string using base58check with the provided prefix version.
+
+  Version option argument is mandatory. The prefix can be a binary,
+  integer or a list of bytes (see examples). All available prefixes are
+  coded in the module `Area58check.Prefixes`.
+
   ## Examples
 
       iex> privkey = "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" |> Base.decode16!()
@@ -25,7 +70,7 @@ defmodule Area58check do
         version: :wif,
         version_bin: <<128>>}
 
-  The version can be indicated in several alternative ways:
+  The version prefix can be indicated in several alternative ways:
 
       iex> privkey = "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" |> Base.decode16!()
       iex> encode(privkey, version: <<128>>)
@@ -52,15 +97,15 @@ defmodule Area58check do
 
   Example to generate an uncompresed bitcoin address from public key
 
-    iex> privkey = "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" |> Base.decode16!()
-    iex> {uncompressed_pubkey, _priv_key} = :crypto.generate_key(:ecdh, :crypto.ec_curve(:secp256k1), privkey)
-    iex> uncompressed_pubkey = :crypto.hash(:ripemd160, :crypto.hash(:sha256, uncompressed_pubkey))
-    iex> encode(uncompressed_pubkey, version: <<0>>)
-    %Area58check{
-      encoded: "1CLrrRUwXswyF2EVAtuXyqdk4qb8DSUHCX",
-      decoded: <<124, 106, 230, 190, 9, 150, 81, 133, 169, 75, 13, 161, 139, 201, 42, 157, 252, 238, 97, 23>>,
-      version: :p2pkh,
-      version_bin: <<0>>}
+      iex> privkey = "0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF" |> Base.decode16!()
+      iex> {uncompressed_pubkey, _priv_key} = :crypto.generate_key(:ecdh, :crypto.ec_curve(:secp256k1), privkey)
+      iex> uncompressed_pubkey = :crypto.hash(:ripemd160, :crypto.hash(:sha256, uncompressed_pubkey))
+      iex> encode(uncompressed_pubkey, version: <<0>>)
+      %Area58check{
+        encoded: "1CLrrRUwXswyF2EVAtuXyqdk4qb8DSUHCX",
+        decoded: <<124, 106, 230, 190, 9, 150, 81, 133, 169, 75, 13, 161, 139, 201, 42, 157, 252, 238, 97, 23>>,
+        version: :p2pkh,
+        version_bin: <<0>>}
 
   If version is an atom, and is not recognized returns error:
 
@@ -80,6 +125,12 @@ defmodule Area58check do
   end
 
   @doc """
+  Decodes a previously encoded string using base58check returning the
+  used prefixed version, the original binary.
+
+  An error message will be returned in case there was a checksum error
+  or a character is not valid.
+
   ## Examples:
 
   Decode WIP key and get the hexadecimal private key
@@ -95,18 +146,18 @@ defmodule Area58check do
 
   If any character of the key is wrong (`J`), checksum will be invalid:
 
-     iex> decode("5JpneLQNKrcznVCQpzodYwAmZ4AoHeyjuRf9iAHAa498rP5kuWb")
-     {:error, :checksum_incorrect}
+      iex> decode("5JpneLQNKrcznVCQpzodYwAmZ4AoHeyjuRf9iAHAa498rP5kuWb")
+      {:error, :checksum_incorrect}
 
   The null string means the checksum is nor present, nor valid:
 
-     iex> decode("")
-     {:error, :checksum_incorrect}
+      iex> decode("")
+      {:error, :checksum_incorrect}
 
   If any character is not a base58 character (Ex: `0`)
 
-     iex> decode("50pneLQNKrcznVCQpzodYwAmZ4AoHeyjuRf9iAHAa498rP5kuWb")
-     {:error, :incorrect_base58}
+      iex> decode("50pneLQNKrcznVCQpzodYwAmZ4AoHeyjuRf9iAHAa498rP5kuWb")
+      {:error, :incorrect_base58}
   """
   @spec decode(String.t) :: {:ok, Area58check.t} | {:error, atom()}
   def decode(payload) do
